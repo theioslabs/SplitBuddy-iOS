4ad482ff07a1c6da2b811bbcfbf9943ed0b1827d
apnakhushal <khushaljhalani@gmail.com>
2025-09-12 02:24:59 +0400

    initial project setup code

---
+++ SplitBuddy/SplitBuddy/SplitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitBuddyApp.swift
+//  SplitBuddy
+//
+//  Created by Khushal on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SplitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
